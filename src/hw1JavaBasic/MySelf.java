4b32b1d448db9e3d0e76ed2c707151442b307c6c
Muhammad Mamun <muhammadmmamun@gmail.com>
2023-06-07 23:48:13 -0400

    HomeWork

---
+++ src/hw1JavaBasic/MySelf.java
@@ -0,0 +1,36 @@
+package hw1JavaBasic;
+
+/*
+ Naming convention:
+ 	Project names usually start with UpperCase --> May2023CoreJava
+ 	Package names usually start with lowerCase --> hw1JavaBasic
+	Class names usually start with UpperCase --> MySelf
+
+ 	Projects, Package, and Class names follow the Camel case or Snake case feature.
+
+	Camel Case: from second word start with UppeCas --> hw1JavaBasic
+ 	Camel Case feature start from second word, not from the first word 
+	
+	Snake Case: alternative way of Camel Case, which start with lowerCase
+	and each word separated by UnderScore --> hw1_java_basic
+*/
+public class MySelf {
+	
+	// main method -- Entry point of Java
+	// To use the main method by shortcut
+	// I have to type --> syso + Enter
+	
+	public static void main(String[] args) {
+		System.out.println("I'm Muhammad Mamun");
+		System.out.println("I'm Muhammad Mamun");
+		
+		// To copy and paste a line by shortcut
+		// I have to type --> alt (option in Mac) + Control/Command + down/up arrow
+		
+		System.out.println("669 Jerome St, Brooklyn, NY-11207");
+		System.out.println("669 Jerome St, Brooklyn, NY-11207");
+		System.out.println("669 Jerome St, Brooklyn, NY-11207");
+
+	}
+
+}
---
+++ src/hw2JavaBasic/Mycar.java
@@ -0,0 +1,13 @@
+package hw2JavaBasic;
+
+public class Mycar {
+
+	public static void main(String[] args) {
+		System.out.print("My name is \"Muhammad Mamun\"" + "\nI love to drive" + "\nMy car info is below: \n");
+		System.out.print();
+		System.out.println("\n\t");
+		// TODO Auto-generated method stub
+
+	}
+
+}
